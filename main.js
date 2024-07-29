93d80bf41277d21279aad49fe580c6e40547164c
Quangteo313 <167507697+Quangteo313@users.noreply.github.com>
2024-07-29 21:27:12 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+// main.js
+
+function showDetails(item) {
+    if(item === 'garan') {
+        window.location.href = 'garan_details.html';
+    } else if(item === 'banh') {
+        window.location.href = 'banh_details.html';
+    } else if(item === 'garan2') {
+        window.location.href = 'garan_details.html';
+    }
+}
